2748eeecff9ad9df97311ed452994c0c2ba3c370
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2025-07-24 12:33:22 -0700

    Update disk-config.nix

--- disk-config.nix
+++ disk-config.nix
@@ -23,10 +23,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = 
-                [
-                  "defaults"
-                ];
+                mountOptions = [ "defaults" ];
               };
             };
             luks = 
@@ -37,80 +34,41 @@
               {
                 type = "luks";
                 name = "cryptroot";
-                extraOpenArgs = 
-                [
-                  "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
+                extraOpenArgs = [ "--allow-discards" "--perf-no_read_workqueue" "--perf-no_write_workqueue" ];
                 settings = 
                 {
-                  crypttabExtraOpts = 
-                  [
-                    "tpm2-device=auto" 
-                    "token-timeout=15"
-                  ];
+                  crypttabExtraOpts = [ "tpm2-device=auto" "token-timeout=15" ];
                 };
                 content = 
                 {
                   type = "btrfs";
-                  extraArgs = 
-                  [
-                    "-L" 
-                    "nixos" 
-                    "-f"
-                  ];
+                  extraArgs = [ "-L" "nixos" "-f" ];
                   subvolumes = 
                   {
                     "/root" = 
                     {
                       mountpoint = "/";
-                      mountOptions = 
-                      [
-                        "subvol=root" 
-                        "compress=zstd" 
-                        "noatime"
-                      ];
+                      mountOptions = [ "subvol=root" "compress=zstd" "noatime" ];
                     };
                     "/home" = 
                     {
                       mountpoint = "/home";
-                      mountOptions = 
-                      [
-                        "subvol=home" 
-                        "compress=zstd" 
-                        "noatime"
-                      ];
+                      mountOptions = [ "subvol=home" "compress=zstd" "noatime" ];
                     };
                     "/nix" = 
                     {
                       mountpoint = "/nix";
-                      mountOptions = 
-                      [
-                        "subvol=nix" 
-                        "compress=zstd" 
-                        "noatime"
-                      ];
+                      mountOptions = [ "subvol=nix" "compress=zstd" "noatime" ];
                     };
                     "/persist" = 
                     {
                       mountpoint = "/persist";
-                      mountOptions = 
-                      [
-                        "subvol=persist" 
-                        "compress=zstd" 
-                        "noatime"
-                      ];
+                      mountOptions = [ "subvol=persist" "compress=zstd" "noatime" ];
                     };
                     "/log" = 
                     {
                       mountpoint = "/var/log";
-                      mountOptions = 
-                      [
-                        "subvol=log" 
-                        "compress=zstd" 
-                        "noatime"
-                      ];
+                      mountOptions = [ "subvol=log" "compress=zstd" "noatime" ];
                     };
                     "/swap" = 
                     {
