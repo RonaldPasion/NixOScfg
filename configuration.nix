a259bdbcf20d5406ea4779d381377c265da3acb1
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2025-07-25 21:44:10 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -55,8 +55,6 @@
       {
         START_CHARGE_THRESH_BAT0 = 75;
         STOP_CHARGE_THRESH_BAT0 = 80;
-        START_CHARGE_THRESH_BAT1 = 75;
-        STOP_CHARGE_THRESH_BAT1 = 80;
       };
     };
   };
